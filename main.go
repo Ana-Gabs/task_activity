f0db69129f18e423e360a822c15bf29db2c057fa
Ana-Gabs <2022371069@uteq.edu.mx>
2025-06-06 16:17:09 -0600

    "Prueba de conección a Mongo"

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"task_activity/config"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	config.ConnectDB()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("API corriendo y DB conectada")
+	})
+
+	app.Listen("Servidor escuchando en el puerto :3000")
+}
