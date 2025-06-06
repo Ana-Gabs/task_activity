a4a14f365d42db0e4ddbd75d82ada5796058e85c
Ana-Gabs <2022371069@uteq.edu.mx>
2025-06-06 16:02:24 -0600

    "conexion a la base de datos"

--- config/db.go
+++ config/db.go
@@ -3,8 +3,10 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -12,15 +14,27 @@ import (
 var DB *mongo.Database
 
 func ConnectDB() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error cargando archivo .env")
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("MONGO_DB")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	DB = client.Database("task_activity")
-};
+
+	DB = client.Database(dbName)
+	log.Println("Conección abierta a MongoDB")
+}
